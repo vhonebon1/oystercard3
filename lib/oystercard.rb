1c6f7bba188b331bd28f687c81b8273d5000257f
jinimcoroneo <jinim1983@gmail.com>
2017-06-13 13:13:07 +0100

    raising an error when insufficient balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,15 @@
 # lib/oystercard.rb
 class Oystercard
-  attr_reader :balance, :status
+  attr_reader :balance, :status, :minimum_balance
   DEFAULT_BALANCE = 0
   MAX_LIMIT = 90
+  MIN_BALANCE = 1
 
-  def initialize(balance = DEFAULT_BALANCE, status = :not_in_use)
-    @balance = balance
+  def initialize(balance = DEFAULT_BALANCE, max_limit = MAX_LIMIT, min_balance = MIN_BALANCE, status = :not_in_use)
     @status = status
+    @balance = balance
+    @max_limit = max_limit
+    @min_balance = min_balance
   end
 
   def top_up(money)
@@ -23,6 +26,7 @@ class Oystercard
   end
 
   def touch_in
+    raise "Card does not have the #{MIN_BALANCE} minimum balance, must top up first" unless has_min_balance?
     @status = :in_use
   end
 
@@ -33,6 +37,10 @@ class Oystercard
 private
 
   def limit_reached?(money)
-    @balance + money > MAX_LIMIT
+    @balance + money > @max_limit
+  end
+
+  def has_min_balance?
+    @balance >= @min_balance
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,10 @@
 require 'oystercard.rb'
 
+
 describe Oystercard do
+
+  let(:topped_up_card) { described_class.new(10) }
+
   describe '#initialize' do
     it 'starts with a default balance' do
       expect(subject.balance).to eq 0
@@ -11,26 +15,22 @@ describe Oystercard do
     end
   end
 
-  context 'Using a topped up card' do
-
-    let(:topped_up_card) { described_class.new(10)}
-
-    describe '#top_up' do
-      it 'adds money to the oystercard balance' do
-        expect(topped_up_card.top_up(9)).to eq 19
-      end
-
-      it 'doesn\'t allow to top up beyond the limit' do
-        expect { topped_up_card.top_up(90) }.to raise_error("Exceeded £#{Oystercard::MAX_LIMIT} limit")
-      end
+  describe '#top_up' do
+    it 'adds money to the oystercard balance' do
+      expect(topped_up_card.top_up(9)).to eq 19
     end
 
-    describe '#deduct' do
-      it 'should deduct money from the balance' do
-        expect(topped_up_card.deduct(5)).to eq 5
-      end
+    it 'doesn\'t allow to top up beyond the limit' do
+      expect { topped_up_card.top_up(90) }.to raise_error("Exceeded £#{Oystercard::MAX_LIMIT} limit")
     end
   end
+
+  describe '#deduct' do
+    it 'should deduct money from the balance' do
+      expect(topped_up_card.deduct(5)).to eq 5
+    end
+  end
+
 
   describe '#in_journey?' do
     it 'checks if the card is in use' do
@@ -40,12 +40,16 @@ describe Oystercard do
 
   describe '#touch_in' do
     it 'can be touched in' do
-      expect(subject).to respond_to :touch_in
+      expect(topped_up_card).to respond_to :touch_in
     end
 
     it 'changes the card status as in use' do
-      subject.touch_in
-      expect(subject.status).to eq :in_use
+      topped_up_card.touch_in
+      expect(topped_up_card.status).to eq :in_use
+    end
+
+    it 'doesn\'t allow you to touch in without a minimum balance' do
+      expect{ subject.touch_in }.to raise_error("Card does not have the #{Oystercard::MIN_BALANCE} minimum balance, must top up first")
     end
   end
 
@@ -55,9 +59,9 @@ describe Oystercard do
     end
 
     it 'changes the card status as not in use' do
-      subject.touch_in
-      subject.touch_out
-      expect(subject.status).to eq :not_in_use
+      topped_up_card.touch_in
+      topped_up_card.touch_out
+      expect(topped_up_card.status).to eq :not_in_use
     end
   end
 end
