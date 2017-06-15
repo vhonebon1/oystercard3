fa5da3eee2a81b553071c631a0cde114af8056d1
Verity <vhonebon@gmail.com>
2017-06-15 12:13:45 +0100

    Added new station class with name and zone

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,4 +2,15 @@ require 'station.rb'
 
 describe Station do
 
+  let(:station) { described_class.new("Bank", 2) }
+
+  describe "#initialize" do
+  it 'initializes with a name' do
+    expect(station.name).to eq("Bank")
+  end
+  it 'initializes with a zone' do
+    expect(station.zone).to eq(2)
+  end
+end
+
 end
